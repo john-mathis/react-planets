5726bccbbce92337abec8bd4e4d4be333fe0dd05
John Mathis <johnmathis98@gmail.com>
2022-10-12 23:24:30 -0400

    added styling to the planet detail component

--- planets/src/Components/Planet Details/PlanetDetails.js
+++ planets/src/Components/Planet Details/PlanetDetails.js
@@ -1,37 +1,45 @@
 import React from "react";
 import "..//Planet Details/PlanetDetails.css";
 
+import img from "../../assets/planet-mercury.svg";
+
 const PlanetDetails = ({ PlanetJSON }) => {
   console.log(PlanetJSON);
 
   const planetPage = PlanetJSON.map((planets) => {
     return (
       <React.Fragment key={planets.name}>
-        <div className="planet-container">
+        <div className="planet-detail-container">
           <picture className="planet-img-container">
-            <img alt="planet" src={planets.images.planet} />
+            <img alt="planet" className="planet-img" src={img} />
           </picture>
 
           <div className="planet-info-container">
             <h2 className="planet-name">{planets.name}</h2>
-            <p className="planets-info">{planets.overview.content}</p>
+            <p className="planet-info">{planets.overview.content}</p>
             <p className="planet-source">
-              Source:<a href={planets.overview.source}>Wikipedia</a>
+              Source:
+              <a href={planets.overview.source}>
+                <span> Wikipedia</span>
+              </a>
             </p>
           </div>
 
-          <div className="planet-stats">
-            <p className="planet-rotation">
-              Rotation time <span>{planets.rotation}</span>
+          <div className="planet-stats-container">
+            <p className="planet-rotation full-border">
+              Rotation time{" "}
+              <span className="planet-stat">{planets.rotation}</span>
             </p>
-            <p className="planet-revolution">
-              Revolutiontime <span>{planets.revolution}</span>
+            <p className="planet-revolution full-border">
+              Revolutiontime{" "}
+              <span className="planet-stat">{planets.revolution}</span>
             </p>
-            <p className="planet-radius">
-              Radius <span>{planets.radius}</span>
+            <p className="planet-radius full-border">
+              Radius <span className="planet-stat">{planets.radius}</span>
             </p>
-            <p className="planet-temperature">
-              Average Temp <span>{planets.temperature}</span>
+            <p className="planet-temperature full-border">
+              Average Temp{" "}
+              <span className="planet-stat">{planets.temperature}</span>
             </p>
           </div>
         </div>
